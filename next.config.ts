e74f81d7b988d5eebc20c14929232f09c467047d
Rafael Augusto <159143529+rafaelaugustodev@users.noreply.github.com>
2026-02-11 22:53:37 -0300

    chore: altera basePath para minúsculas na produção

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/Portfolio' : '',
+  basePath: isProd ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
